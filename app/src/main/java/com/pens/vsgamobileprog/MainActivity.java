fb813e57fece6b2895e0090fea0faa7cfe809918
fauzi <rizael.ichigo28@gmail.com>
2019-07-26 15:53:29 +0700

    Add id on item in activity_main.xml &&  Migrating project to androidx

--- app/src/main/java/com/pens/vsgamobileprog/MainActivity.java
+++ app/src/main/java/com/pens/vsgamobileprog/MainActivity.java
@@ -1,9 +1,8 @@
 package com.pens.vsgamobileprog;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
