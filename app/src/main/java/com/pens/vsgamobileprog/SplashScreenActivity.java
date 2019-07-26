421c897dbf1cd27ae5ff5e08e5618236ebe86ff7
fauzi <rizael.ichigo28@gmail.com>
2019-07-26 15:53:29 +0700

    Add id on item in activity_main.xml

--- app/src/main/java/com/pens/vsgamobileprog/SplashScreenActivity.java
+++ app/src/main/java/com/pens/vsgamobileprog/SplashScreenActivity.java
@@ -3,7 +3,7 @@ package com.pens.vsgamobileprog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
